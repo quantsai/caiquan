230230a331a6bda565442a41227c86ce53e67c6d
caiquan <396022671@qq.com>
2024-09-06 13:49:03 +0800

    Initial Commit

---
+++ caiquan/caiquanApp.swift
@@ -0,0 +1,17 @@
+//
+//  caiquanApp.swift
+//  caiquan
+//
+//  Created by 蔡铨 on 2024/9/6.
+//
+
+import SwiftUI
+
+@main
+struct caiquanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
